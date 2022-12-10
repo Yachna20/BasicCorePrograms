0512ecf139e4024f18ee2db3419eb4a0fc9ce54d
Yachna20 <thakreyachna20@gmail.com>
2022-12-10 15:01:50 +0530

    [Yachna]-Flipcoin

---
+++ BasicCoreCodes/Flipcoin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreCodes
+{
+  internal class Flipcoin
+    {
+        double PercentageOfHead = 0, PercentageOfTail = 0;
+        double HeadCount = 0, TailCount = 0;
+        public void CheckHeadTail()
+        {
+            Console.WriteLine("Enter no. of times you want to flip the coin");
+            int NumofCount = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= NumofCount; i++)
+            {
+                Random randobj = new Random();
+                double randomnumber = randobj.NextDouble();
+
+                if (randomnumber < 0.5)
+                {
+                    TailCount++;
+                    Console.WriteLine("Tail");
+                }
+                else
+                {
+                    HeadCount++;
+                    Console.WriteLine("Head");
+                }
+                Console.WriteLine("Total head count is-" + HeadCount);
+                Console.WriteLine("Total tail count is-" + TailCount);
+                Console.WriteLine("****Percentage of Head and Tail****");
+                PercentageOfHead = HeadCount / NumofCount * 100;
+                PercentageOfTail = TailCount / NumofCount * 100;
+                Console.WriteLine("Percentage of Head" + PercentageOfHead + "%");
+                Console.WriteLine("Percentage of Tail" + PercentageOfTail + "%");
+            }
+
+
+
+        }
+    }
+}
---
+++ BasicCoreCodes/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace BasicCoreCodes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1.Flipcoin");
+                    
+  
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Flipcoin flipcoin = new Flipcoin();
+                        flipcoin.CheckHeadTail();
+                        break;
+                    
+
+
+
+                        
+                    default:
+                        Console.WriteLine("Please enter valid choice");
+                        break;
+                }
+            }
+        }
+
+    }
+}
